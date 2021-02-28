ca4afc1f21a64ef12d2ed86f640028b0ed3364ea
Cristian Bullorini <cristian.bullorini@vnstudios.com>
2021-02-28 17:57:23 -0300

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+     <div class="container-fluid">
+    <b-button v-b-toggle.sidebar-variant>Routes</b-button>
+
+    <b-sidebar id="sidebar-variant" title="Sidebar" bg-variant="dark" text-variant="light" shadow>
+        <div class="px-3 py-2">
+            <p>
+                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+            </p>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+        </div>
+    </b-sidebar>
+ </div> 
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
